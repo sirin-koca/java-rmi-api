438c168ca8365cbcd2d95d48e5e0a189cbc58630
Sirin Koca <sirinkoc@uio.no>
2025-09-06 01:35:56 +0200

    cleanup: prepare main branch for project start

---
+++ src/main/java/org/group5/Main.java
@@ -0,0 +1,16 @@
+
+/*
+IN5020 Autumn 2025 - Assignment 1
+Title: The Java-RMI International Statistics Service
+Goal: Build an object-based distributed system using Java RMI, with client-server architecture,
+proxy-based load balancing, caching, and Docker deployment.
+*/
+
+package org.group5;
+public class Main {
+    public static void main(String[] args) {
+        // Tradisjonstro starter med å printe ut "Hello RMI World!".
+        System.out.println("Hello RMI World!");
+
+    }
+}
