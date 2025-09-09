c63ce6f0756b0ba545cbc0e9a9437a198f7fa978
Mariam Kajo <Maryamkajo2001@hotmail.com>
2025-09-09 21:10:22 +0200

    ProxyInterface and ProxyRegistration for communication with server and client

--- src/main/java/org/group5/api/proxyInterface.java
+++ src/main/java/org/group5/api/proxyInterface.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 //Interface for client and proxy communication
 //The client asks the proxy which erver to use for a given zone
 //It returns the RMI URL of a server for a given zone
-public interface proxyInterface extends Remote {
+public interface ProxyInterface extends Remote {
     String getServerForZone(int clientZone) throws RemoteException;
 }
