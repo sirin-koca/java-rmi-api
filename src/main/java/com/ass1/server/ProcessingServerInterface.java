ff55feaf28ad98f85676750f3859eb3fff538ec6
Stål Brændeland <staalbr@gmail.com>
2025-09-09 20:55:37 +0200

    Naming consistency

---
+++ src/main/java/com/ass1/server/ProcessingServerInterface.java
@@ -0,0 +1,9 @@
+package com.ass1.server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ProcessingServerInterface extends Remote
+{
+    int add(int num1, int num2) throws RemoteException;
+}
