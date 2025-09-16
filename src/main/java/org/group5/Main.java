bebee735af259c2e1439d05421a0707026591c4b
Sirin Koca <sirinkoc@uio.no>
2025-09-16 16:09:26 +0200

    removed redundant server files, some code-clean-up, edit pom.xml JDK 21 for all components.

--- src/main/java/org/group5/Main.java
+++ src/main/java/org/group5/Main.java
@@ -1,6 +1,6 @@
 
 /*
-IN5020 Autumn 2025 - Assignment 1
+IN5020 Autumn 2025 - Assignment1
 Title: The Java-RMI based International Statistics Service
 Goal: Build an object-based distributed system using Java RMI, with client-server architecture,
 proxy-based load balancing, caching, and Docker deployment.
